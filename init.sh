6e39e49ec7b289761d1dd7239054162c84a21e7b
Bisheng Liu <bisheng@umich.edu>
2016-04-11 23:18:59 -0400

    init

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@ rm -f ~/.vimrc
 ln .vimrc ~/.vimrc
 
 rm -f ~/.bash_profile
-cp .bashrc ~/.bash_profile
+ln .bashrc ~/.bash_profile
 
 rm -f ~/.ssh/config
-cp .ssh/config ~/.ssh/config
+ln .ssh/config ~/.ssh/config
 
 rm -f ~/.tmux.conf
-cp .tmuxrc ~/.tmux.conf
+ln .tmuxrc ~/.tmux.conf
 
